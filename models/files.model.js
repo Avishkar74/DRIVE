113bddd8541b2f2ae2fb66bbdeef5e9ef23cbc16
Avishkar74 <avishkarc600@gmail.com>
2025-05-31 11:21:58 +0530

    Adds file model schema for storing file information

---
+++ models/files.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const fileSchema = new mongoose.Schema({
+    path:{
+        type: String,
+        required:[true,'File path is required']
+
+    },
+    originalName:{
+        type: String,
+        required:[true,'File original name is required']
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:[true,'User is required']
+
+    }
+})
